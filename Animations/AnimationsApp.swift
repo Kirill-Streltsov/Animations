c060069565ed01a70b941aec824787d33ffa4d43
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-07-29 13:00:28 +0200

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Kirill Streltsov on 29.07.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
